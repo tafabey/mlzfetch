78e05c81ffb614cb8398c9b00c3885fdd8d6e6e0
tafabey <211373705+tafabey@users.noreply.github.com>
2026-03-12 13:59:18 +0300

    Now program can show kernel

--- logic.py
+++ logic.py
@@ -7,11 +7,24 @@ def read_username():
         return "error"
 
 def read_hostname():
-    with open("/proc/sys/kernel/hostname") as host:
-        return host.read().rstrip("\n")
+    with open("/proc/sys/kernel/hostname") as hostname:
+        return hostname.read().rstrip("\n")
 
-def read_osname():
+def read_os():
     with open("/etc/os-release") as os_release:
         for line in os_release:
             if line.startswith("PRETTY_NAME=\""):
-                return line[13:-1]
+                return line[13:-2]
+
+def read_host():
+    with open("/sys/class/dmi/id/product_name") as host:
+        return host.read().rstrip("\n")
+
+def read_kernel():
+    osname = []
+    with open("/proc/sys/kernel/ostype") as ostype:
+        osname.append(ostype.read().rstrip())
+    with open("/proc/sys/kernel/osrelease") as osrelease:
+        osname.append(osrelease.read().rstrip())
+    return " ".join(osname)
+            
--- main.py
+++ main.py
@@ -4,18 +4,24 @@ def main():
     sysinfo = {
         "username": "",
         "hostname": "",
-        "osname": ""
+        "os": "",
+        "host": "",
+        "kernel": ""
     }
     
     sysinfo["username"] = logic.read_username()
     sysinfo["hostname"] = logic.read_hostname()
-    sysinfo["osname"] = logic.read_osname()
+    sysinfo["os"] = logic.read_os()
+    sysinfo["host"] = logic.read_host()
+    sysinfo["kernel"] = logic.read_kernel()
     
     print(f"{sysinfo['username']}@{sysinfo['hostname']}")
     for _ in range(len(sysinfo["username"]) + len(sysinfo["hostname"]) + 1):
         print("-", end="")
     print()
-    print(f"OS: {sysinfo['osname']}")
+    print(f"OS: {sysinfo['os']}")
+    print(f"Host: {sysinfo['host']}")
+    print(f"Kernel: {sysinfo['kernel']}")
 
 if __name__ == "__main__":
     main()
