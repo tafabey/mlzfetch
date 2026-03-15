d0e69960370d703252684bf1ddfdbaa6580a87ac
tafabey <211373705+tafabey@users.noreply.github.com>
2026-03-15 14:28:12 +0300

    Opening files now read-only

--- platforms/linux.py
+++ platforms/linux.py
@@ -7,31 +7,31 @@ def read_username():
         return "error"
 
 def read_hostname():
-    with open("/proc/sys/kernel/hostname") as hostname:
+    with open("/proc/sys/kernel/hostname", "r") as hostname:
         return hostname.read().rstrip("\n")
 
 def read_os():
-    with open("/etc/os-release") as os_release:
+    with open("/etc/os-release", "r") as os_release:
         for line in os_release:
             if line.startswith("PRETTY_NAME=\""):
                 return line[13:-2]
 
 def read_host():
-    with open("/sys/class/dmi/id/product_name") as host:
+    with open("/sys/class/dmi/id/product_name", "r") as host:
         return host.read().rstrip("\n")
 
 def read_kernel():
     osname = []
-    with open("/proc/sys/kernel/ostype") as ostype:
+    with open("/proc/sys/kernel/ostype", "r") as ostype:
         osname.append(ostype.read().rstrip())
-    with open("/proc/sys/kernel/osrelease") as osrelease:
+    with open("/proc/sys/kernel/osrelease", "r") as osrelease:
         osname.append(osrelease.read().rstrip())
     return " ".join(osname)
             
 def read_uptime():
     hours = 0
     minutes = 0
-    with open("/proc/uptime") as uptime:
+    with open("/proc/uptime", "r") as uptime:
         minutes = int(float(uptime.read().split()[0]) / 60.0)
         hours = int(minutes / 60.0)
         minutes = int(minutes % 60)
@@ -39,5 +39,5 @@ def read_uptime():
 
 def read_shell():
     ppid = os.getppid()
-    with open(f"/proc/{ppid}/comm") as comm:
+    with open(f"/proc/{ppid}/comm", "r") as comm:
         return comm.read().strip()
