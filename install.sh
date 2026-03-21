0a5981478cff9096ce2d1901904fbf5031019081
tafabey <211373705+tafabey@users.noreply.github.com>
2026-03-21 12:17:43 +0300

    Update install.sh for venv

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 mkdir -p ~/.local/share/mlzfetch/
 cp -r logos/ ~/.local/share/mlzfetch/
+python3 -m venv .venv
+source .venv/bin/activate
 pip install .
+cp .venv/bin/mlzfetch ~/.local/bin/
+deactivate
