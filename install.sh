fa4aed843a7e188d8a717012cc6e7430f5fac126
tafabey <211373705+tafabey@users.noreply.github.com>
2026-03-21 12:23:25 +0300

    Undo changes about venv

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 mkdir -p ~/.local/share/mlzfetch/
 cp -r logos/ ~/.local/share/mlzfetch/
-python3 -m venv .venv
-source .venv/bin/activate
 pip install .
-cp .venv/bin/mlzfetch ~/.local/bin/
-deactivate
